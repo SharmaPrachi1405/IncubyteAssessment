8fa9ab8e3e9a61dbe3e7d61433eda8b33cd385bc
Prachi Sharma <sharma2prachi1@gmail.com>
2022-09-04 19:22:34 +0530

    Final Commit

--- Test_String.py
+++ Test_String.py
@@ -1,3 +1,4 @@
+from asyncio.tasks import _T1
 import unittest
 
 from StringCalculator import String_Calculator
@@ -8,8 +9,9 @@ class StringTest(unittest.TestCase):
         result = T1.add("")
         self.assertEquals(0, result)
 
-    test_list = {"1",1, "2",2}
+    
     def ShouldReturnOneForString(word , expectedresult,self):
+        test_list = {"1",1, "2",2}
         T1 = String_Calculator()
         result = T1.add("1")
         for word, expectedresult in test_list:
@@ -22,10 +24,24 @@ class StringTest(unittest.TestCase):
         self.assertEquals(expectedresult, result)
 
 
-    def NumberNegativeThan( word , expectedresult, self):
+    def NumberNegativeThan(  self ,word):
         T1 = String_Calculator()
         result = T1.add(word)
-        self.assertEquals(word<0, "Positive number expected")
+        para_list = [-1, 0]
+        for i in para_list:
+            if(i<0):
+                return 0
+        self.assertLess(word<0, "Positive number expected")
+
+    
+    def MultipleNegatives(self , word):
+        Negative_list=[0,-1,-2]
+        T1=String_Calculator()
+        result = T1.add(word)
+        for i in Negative_list:
+           if(i<0):
+            print(i)
+            self.assertIn(i<0, "Negative numbers")
 
     def NumberBiggerThan( word , expectedresult, self):
         T1 = String_Calculator()
