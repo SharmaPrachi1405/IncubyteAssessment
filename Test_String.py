6f4658870648b5182c979dd9d825dad2c2b25bf4
Prachi Sharma <sharma2prachi1@gmail.com>
2022-09-05 00:32:17 +0530

    Test 1

--- Test_String.py
+++ Test_String.py
@@ -2,6 +2,10 @@ import unittest
 
 from StringCalculator import String_Calculator
 
-def test_string_calc_should_return_zero_on_empty_string():
+def return_zero_on_empty_string():
     result = add("")
     assert result == 0, "String calculater should return 0 on empty string"
+
+def return_correct_value_on_one():
+    result = add("1")
+    assert result == 1, "String calculater should return 1 on \"1\" string"
