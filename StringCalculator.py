ff0f3817d97c5806e38118264301fc2fa00c28fd
Prachi Sharma <sharma2prachi1@gmail.com>
2022-09-04 19:01:07 +0530

    more than 1000 value ignored

--- StringCalculator.py
+++ StringCalculator.py
@@ -5,8 +5,14 @@ class String_Calculator:
             return 0
         if(',' in word):
             return 3 
+
         else:
             return int(word)
+
+        para_list = [-1, 0]
+        for i in para_list:
+            if(i<0):
+                return 0
 
 
 
--- Test_String.py
+++ Test_String.py
@@ -8,13 +8,30 @@ class StringTest(unittest.TestCase):
         result = T1.add("")
         self.assertEquals(0, result)
 
-    test_list = ["1",1]
-    def ShouldReturnOneForString(self, word , expectedresult):
+    test_list = {"1",1, "2",2}
+    def ShouldReturnOneForString(word , expectedresult,self):
         T1 = String_Calculator()
-        result = T1.add(word)
-        self.assertEquals(expectedresult, result)
+        result = T1.add("1")
+        for word, expectedresult in test_list:
+            with self.subTest():
+                self.assertEquals(expectedresult, result)
 
     def ShouldReturnSumForString(self, word , expectedresult):
         T1 = String_Calculator()
         result = T1.add(word)
+        self.assertEquals(expectedresult, result)
+
+
+    def NumberNegativeThan( word , expectedresult, self):
+        T1 = String_Calculator()
+        result = T1.add(word)
+        self.assertEquals(word<0, "Positive number expected")
+
+    def NumberBiggerThan( word , expectedresult, self):
+        T1 = String_Calculator()
+        result = T1.add(word)
+        for i in word :
+            if(word > 1000):
+                temp = word
+                i+=1
         self.assertEquals(expectedresult, result)
