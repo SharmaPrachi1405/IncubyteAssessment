ed190a629a5d7444a5cf93d3bbfb1d940b1f003d
Prachi Sharma <sharma2prachi1@gmail.com>
2022-09-05 00:43:19 +0530

    Test3

--- StringCalc.py
+++ StringCalc.py
@@ -12,16 +12,10 @@ class String_Calculator:
         param = normalize_custom_delimiter(param)
         return param.replace("\n", ",").strip(',')
 
-    def should_return_correct_value_on_two():
-        result = add("2")
-        assert result == 2, "String calculater should return 2 on \"2\" string"
-
-    def should_add_two_numbers():
-        result = add("1,2")
-        assert result == 3, "String calculater should return 3 for \"1,2\" string"
-
-        result = add("5,10")
-        assert result == 15, "String calculater should return 15 for \"5,10\" string"
+    def add_numbers(param: str) -> int:
+        numbers = list(map(int, param.split(",")))
+        validate_numbers(numbers)
+        return sum(numbers)
 
     def validate_numbers(param: str):
         if any(num < 0 for num in param):
--- String_Test.py
+++ String_Test.py
@@ -10,7 +10,21 @@ def return_correct_value_on_one():
     result = add("1")
     assert result == 1, "String calculater should return 1 on \"1\" string"
 
-def add_numbers(param: str) -> int:
-    numbers = list(map(int, param.split(",")))
-    validate_numbers(numbers)
-    return sum(numbers)
+
+
+def should_return_correct_value_on_two():
+    result = add("2")
+    assert result == 2, "String calculater should return 2 on \"2\" string"
+
+def should_add_two_numbers():
+    result = add("1,2")
+    assert result == 3, "String calculater should return 3 for \"1,2\" string"
+
+    result = add("5,10")
+    assert result == 15, "String calculater should return 15 for \"5,10\" string"
+
+def test_string_calc_negative_number_exception_check():
+    try:
+        add("1\n-2,3")
+    except ValueError as e:
+        assert str(e) == "negatives not allowed [-2]"
