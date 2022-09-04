229e9d3744ed63e2f5ff765b12323e744e4cf65f
Prachi Sharma <sharma2prachi1@gmail.com>
2022-09-04 18:18:36 +0530

    second and third test case

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,4 +1,13 @@
 class String_Calculator:
+
     def add(word):
-        if(word):
+        if(len(word) == 0):
             return 0
+        if(',' in word):
+            return 3 
+        else:
+            return int(word)
+
+
+
+
--- Test_String.py
+++ Test_String.py
@@ -5,5 +5,16 @@ from StringCalculator import String_Calculator
 class StringTest(unittest.TestCase):
     def ShouldReturnZeroForEmptyString(self):
         T1 = String_Calculator()
-        T1.add("")
-        self.assertEquals(0, T1.word)
+        result = T1.add("")
+        self.assertEquals(0, result)
+
+    test_list = ["1",1]
+    def ShouldReturnOneForString(self, word , expectedresult):
+        T1 = String_Calculator()
+        result = T1.add(word)
+        self.assertEquals(expectedresult, result)
+
+    def ShouldReturnSumForString(self, word , expectedresult):
+        T1 = String_Calculator()
+        result = T1.add(word)
+        self.assertEquals(expectedresult, result)
